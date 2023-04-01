29cc90b2878e2b85d409ede5edc5d4d5b9512022
Chris J <chris.janssens2@gmail.com>
2023-04-01 05:53:08 -0500

    add rapier 2d with a simple dynamic rigidbody

---
+++ src/environment/platforms.rs
@@ -0,0 +1,54 @@
+use crate::prelude::*;
+
+pub struct PlatformsPlugin;
+
+impl Plugin for PlatformsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_platforms);
+    }
+}
+
+fn spawn_platforms(mut cmd: Commands, win: Query<&Window>) {
+    let window = win.single();
+    //bottom
+    cmd.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::RED,
+                custom_size: Some(Vec2::new(window.width(), 50.0)),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(
+                0.0,
+                -(window.height() / 2.0) + 25.0,
+                100.0,
+            )),
+            ..Default::default()
+        },
+        Collider::cuboid(window.width() / 2.0, 25.0),
+    ));
+    //left
+    cmd.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::RED,
+            custom_size: Some(Vec2::new(50.0, window.height())),
+            ..Default::default()
+        },
+        transform: Transform::from_translation(Vec3::new(
+            -(window.width() / 2.0) + 25.0,
+            0.0,
+            100.0,
+        )),
+        ..Default::default()
+    });
+    //right
+    cmd.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::RED,
+            custom_size: Some(Vec2::new(50.0, window.height())),
+            ..Default::default()
+        },
+        transform: Transform::from_translation(Vec3::new(window.width() / 2.0 - 25.0, 0.0, 100.0)),
+        ..Default::default()
+    });
+}
---
+++ src/player/player.rs
@@ -0,0 +1,25 @@
+use crate::prelude::*;
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_player);
+    }
+}
+
+fn spawn_player(mut cmd: Commands) {
+    cmd.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::GREEN,
+                custom_size: Some(Vec2::new(40.0, 40.0)),
+                ..Default::default()
+            },
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        },
+        Collider::cuboid(20.0, 20.0),
+        RigidBody::Dynamic,
+    ));
+}
