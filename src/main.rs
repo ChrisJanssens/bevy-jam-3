3ea50422b8705f665c65374142cb5a9e2d6d0cef
Chris J <chris.janssens2@gmail.com>
2023-04-01 05:54:22 -0500

    these were missing from the last commit :D

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,29 @@
 // workflow treats them as errors, so this allows them throughout the project.
 // Feel free to delete this line.
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
+mod environment;
+mod player;
 
-use bevy::prelude::*;
+mod prelude {
+    pub use crate::environment::platforms::*;
+    pub use crate::player::player::*;
+    pub use bevy::prelude::*;
+    pub use bevy_rapier2d::prelude::*;
+}
+
+use prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(40.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .add_plugin(PlatformsPlugin)
+        .add_plugin(PlayerPlugin)
         .add_startup_system(setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("icon.png"),
-        ..Default::default()
-    });
 }
