8ed83e53bcff7ab7083aeec6e50159cb6500f524
Chris Janssens <chris.janssens2@gmail.com>
2023-04-08 07:02:41 -0500

    add idle animation

--- src/control/animate.rs
+++ src/control/animate.rs
@@ -2,12 +2,26 @@ use crate::prelude::*;
 
 #[derive(Component)]
 pub struct PlayerAnimation {
+    pub idle_range: (usize, usize),
     pub walk_range: (usize, usize),
     pub jump_range: (usize, usize),
     pub timer: Timer,
 }
 
-pub fn animate_player_idle() {}
+pub fn animate_player_idle(
+    mut player_anim: Query<(&mut TextureAtlasSprite, &mut PlayerAnimation), With<Player>>,
+    time: Res<Time>,
+) {
+    for (mut atlas, mut anim) in player_anim.iter_mut() {
+        if anim.timer.tick(time.delta()).finished() {
+            let mut i = atlas.index + 1;
+            if i > anim.idle_range.1 {
+                i = anim.idle_range.0;
+            }
+            atlas.index = i;
+        }
+    }
+}
 
 pub fn animate_player_walking(
     mut player_anim: Query<(&mut TextureAtlasSprite, &mut PlayerAnimation), With<Player>>,
@@ -36,8 +50,9 @@ pub fn animate_player_jumping(
         if anim.timer.tick(time.delta()).finished() {
             let i = atlas.index + 1;
             if i == anim.jump_range.1 {
-                vel.linvel = Vec2::new(0.0, 400.0);
                 next_player_state.set(PlayerState::JumpingInAir);
+            } else if i == anim.jump_range.1 - 1 {
+                vel.linvel = Vec2::new(0.0, 400.0);
             }
             atlas.index = i;
         }
--- src/control/player.rs
+++ src/control/player.rs
@@ -33,12 +33,13 @@ fn spawn_player(
 ) {
     let texture_handle = asset_server.load("gary_walking_spritesheet.png");
     let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(27.0, 45.0), 4, 2, None, None);
+        TextureAtlas::from_grid(texture_handle, Vec2::new(27.0, 45.0), 4, 3, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     cmd.spawn((
         SpriteSheetBundle {
             sprite: TextureAtlasSprite {
+                index: 7,
                 custom_size: Some(Vec2::new(54.0, 90.0)),
                 ..Default::default()
             },
@@ -59,6 +60,7 @@ fn spawn_player(
         Player,
         LockedAxes::ROTATION_LOCKED,
         PlayerAnimation {
+            idle_range: (8, 9),
             walk_range: (0, 3),
             jump_range: (4, 7),
             timer: Timer::new(Duration::from_millis(200), TimerMode::Repeating),
@@ -78,18 +80,13 @@ fn move_player(
     outputs: Query<&KinematicCharacterControllerOutput>,
     keyboard_input: Res<Input<KeyCode>>,
     mut next_player_state: ResMut<NextState<PlayerState>>,
+    cur_state: Res<State<PlayerState>>,
 ) {
     let (mut player_controller, mut player, play_anim) = controllers.single_mut();
     let mut movement = Vec2::new(0.0, 0.0);
 
     let ground_touched = outputs.iter().map(|p| p.grounded).any(|t| t);
-    if keyboard_input.just_pressed(KeyCode::A) || keyboard_input.just_pressed(KeyCode::D) {
-        next_player_state.set(PlayerState::Walking);
-    } else if (keyboard_input.just_released(KeyCode::A) && !keyboard_input.pressed(KeyCode::D))
-        || (!keyboard_input.pressed(KeyCode::A) && keyboard_input.just_released(KeyCode::D))
-    {
-        next_player_state.set(PlayerState::Idle);
-    }
+
     if keyboard_input.pressed(KeyCode::A) {
         movement += Vec2::new(-1.0, 0.0);
         player.flip_x = true;
@@ -98,9 +95,29 @@ fn move_player(
         movement += Vec2::new(1.0, 0.0);
         player.flip_x = false;
     }
-    if keyboard_input.just_pressed(KeyCode::W) && ground_touched {
-        player.index = play_anim.jump_range.0;
-        next_player_state.set(PlayerState::Jumping);
+
+    if ground_touched {
+        if keyboard_input.just_pressed(KeyCode::A) || keyboard_input.just_pressed(KeyCode::D) {
+            player.index = play_anim.walk_range.0;
+            next_player_state.set(PlayerState::Walking);
+        } else if (keyboard_input.just_released(KeyCode::A) && !keyboard_input.pressed(KeyCode::D))
+            || (!keyboard_input.pressed(KeyCode::A) && keyboard_input.just_released(KeyCode::D))
+        {
+            player.index = play_anim.idle_range.0;
+            next_player_state.set(PlayerState::Idle);
+        } else if cur_state.0 == PlayerState::JumpingInAir {
+            if keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::D) {
+                player.index = play_anim.walk_range.0;
+                next_player_state.set(PlayerState::Walking); // hit the ground walking
+            } else {
+                player.index = play_anim.idle_range.0;
+                next_player_state.set(PlayerState::Idle);
+            }
+        }
+        if keyboard_input.just_pressed(KeyCode::W) {
+            player.index = play_anim.jump_range.0;
+            next_player_state.set(PlayerState::Jumping);
+        }
     }
     player_controller.translation = Some(movement);
 }
